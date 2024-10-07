40f47ff28a63bb109ee19e152187c986f56caad3
Ali <alimahsim@gmail.com>
2024-10-07 18:14:27 +0500

    Started JS

---
+++ 27-Intro_JS/main.js
@@ -0,0 +1,17 @@
+console.log("Hello world!")
+
+// Variables = Container which contains the value
+// var, let, const, and not mantioning the variable
+
+let name = "Ali"
+
+// let = keyword creates variable, 
+// name = Indentifier (variable name),
+// "Ali" = variable value,
+// = is operator 
+
+
+// Data types
+// There are 8 data types in JS
+
+// String
