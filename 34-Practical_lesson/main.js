0652fb6e4108d671cb4596f48b3fe257d3419aec
Ali <alimahsim@gmail.com>
2025-09-02 17:52:04 +0500

    modify 34-lesson

--- 34-Practical_lesson/main.js
+++ 34-Practical_lesson/main.js
@@ -1,104 +1,151 @@
-// // Letter counter function
+// Practices
 
-// function vowelCounter(w) {
-//   let counter = 0;
-//   for (i = 1; i <= w.length; i++) {
-//     counter++;
-//   }
-//   console.log(counter);
+// 1
+const number = 29;
+// console.log(number);
+
+let numToStr = number.toString();
+// console.log(numToStr);
+
+let arrNumber = numToStr.split("");
+// console.log(arrNumber);
+
+// if (arrNumber[0] > arrNumber[1] || arrNumber[0] === arrNumber[1]) {
+//   console.log(true);
+// } else {
+//   console.log(false);
 // }
-// vowelCounter("Counttyside"); // 11
 
-// Creating a function that counts vowel letters
-// 1 version
+// Creating function for these
 
-// let vowels = ["a", "A", "o", "O", "e", "E", "u", "U", "i", "I"];
-// function vowelCounter(w) {
-//   let counter = 0;
-//   for (i = 0; i < w.length; i++) {
-//     for (let j = 0; j < vowels.length; j++) {
-//       if (w[i] === vowels[j]) counter++;
-//     }
-//   }
-//   console.log(counter);
-// }
-// vowelCounter("Web developer"); // 5
+function largestSwap(n) {
+  let numToStr = n.toString();
+  let arrNumber = numToStr.split("");
 
-// // 2nd solution
-// function vowelCounter(w) {
-//   let vowels = ["a", "A", "o", "O", "e", "E", "u", "U", "i", "I"];
-//   let counter = 0;
-//   for (let i = 0; i < w.length; i++) {
-//     if (vowels.includes(w[i])) {
-//       counter++;
-//     }
-//   }
-//   console.log(counter);
-// }
-// vowelCounter("Web developer"); // 5
+  if (arrNumber[0] > arrNumber[1] || arrNumber[0] === arrNumber[1]) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+// largestSwap(33) // true
+// largestSwap(73) // true
+// largestSwap(13) // false
 
-// To be continued ...
-const numbers = 121255;
+function largeSwap(n) {
+  return Number(n.toString()[0]) >= Number(n.toString()[1]);
+}
+// console.log(largeSwap(10));
 
-let r = numbers.toString().split("");
-console.log(r);
-console.log(typeof r); // object
-console.log(typeof r[0]); // string
+// 2
+function drop(arr, num) {
+  return arr.slice(num);
+}
+console.log(drop([1, 2, 3], 1));
+console.log(drop([1, 2, 3, 100, 5], 5));
+console.log(drop([1, 2, 3, 100, 5], 3));
 
-// By a function
-let nums = 445;
+// 3
+//  Convert Time 09:35 PM
 
-function toArray(num) {
-  let r = num.toString().split("").map(Number);
+// 09:35 PM => 21:35
+// 09:35 AM => 09:35
+
+function convertTime(time) {
+  let r = "";
+  // console.log(hour);
+  if (time.split(" ")[1] === "PM") {
+    let hour = time.slice(0, 2);
+    let hourNum = Number(hour) + 12;
+    if (hourNum === 24) {
+      hourNum = "00";
+    }
+    r = hourNum + time.slice(2, 5);
+  } else {
+    r = time.slice(0, 6);
+  }
   return r;
 }
-console.log(toArray(nums)); // [4, 4, 5]
+console.log(convertTime("09:35 PM"));
+console.log(convertTime("09:35 AM"));
+console.log(convertTime("12:00 AM"));
+console.log(convertTime("12:00 PM"));
 
-// Divides evenly
+// 4
+function getNumsToN(n) {
+  let r = [];
+  for (let i = 1; i < n; i++) {
+    r.push(i);
+  }
+  return r;
+}
+console.log(getNumsToN(9));
 
-let dividesEvenly = (a, b) => (a % b === 0 ? true : false);
-console.log(dividesEvenly(11, 7)); // false
-console.log(dividesEvenly(12, 4)); // true
+// 5
+let name = "Alibek";
+let surname = "Nematov";
 
-// Divides by Five
+function findVawels(str) {
+  let strToArr = str.split("");
+  // console.log(strToArr);
+  let vowels = ["A", "a", "O", "o", "E", "e", "U", "u", "I", "i"];
+  let counter = 0;
+  for (let i = 0; i <= strToArr.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (strToArr[i] === vawels[j]) {
+        counter++;
+      }
+    }
+  }
+  return counter;
+}
+// console.log(findVawels(name));
+// console.log(findVawels(surname));
 
-const divieFive = (a) => a % 5 === 0;
-console.log(divieFive(20)); // true
-console.log(divieFive(21)); // false
+// 5
 
-// String to number and calculater the value
+function findVawels(str) {
+  let strToArr = str.split("");
+  // console.log(strToArr);
+  let vowels = ["A", "a", "O", "o", "E", "e", "U", "u", "I", "i"];
+  let counter = 0;
+  strToArr.forEach(function (el) {
+    vowels.forEach(function (item) {
+      if (el === item) {
+        counter++;
+      }
+    });
+  });
+  return counter;
+}
+// console.log(findVawels(name));
+// console.log(findVawels(surname));
 
-const equation = function (n) {
-  return eval(n);
-};
-console.log(equation("35 + 35")); // 70
-console.log(equation("35 - 35")); // 0
+// 5
 
-console.log(eval("35/5")); // 7
+function findVawels(str) {
+  let counter = 0;
+  let vowels = ["A", "a", "O", "o", "E", "e", "U", "u", "I", "i"];
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      counter++;
+    }
+  }
+  return counter;
+}
+// console.log(findVawels(name));
+// console.log(findVawels(surname));
 
-
-console.log(isNaN("")); //false
-console.log(true-'hello') // NaN
-console.log(typeof(null + 40)) // number
-let v = !!"0";
-console.log(v); // true
-console.log(1.15 + 2.30)  // 3.4499999999999997
-console.log(22 - "1"); //21
-console.log(typeof (function () {})()); //undefined 
-console.log(Boolean("0") == Boolean(0)); //false
-console.log(null == undefined); // true
-console.log(Boolean('0')) // true
-console.log(typeof function(){}) //function
-console.log(typeof Infinity);//number
-console.log((0 && "") || []); //[]
-console.log(typeof (function () {})()); //undefined
-// false values 
-console.log(true+'hello') // truehello
-console.log(typeof alert) //undefined
-console.log(0 || (1 && 2) || 3); //2
-console.log(2 + "2"); //22
-console.log(typeof Symbol("JS")); //symbol
-console.log('123' == 123);
-
-console.log(0 && "" && []);
-console.log("0")
+// 5
+function findVawels(str) {
+  let counter = 0;
+  let vowels = ["A", "a", "O", "o", "E", "e", "U", "u", "I", "i"];
+  for (let i = 0; i < vowels.length; i++) {
+    if (str.includes(vowels[i])) {
+      counter++;
+    }
+  }
+  return counter;
+}
+console.log(findVawels(name));
+console.log(findVawels(surname));
